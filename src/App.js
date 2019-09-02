556f8a6226736b16511dcba9b35a64b4ddbf82e8
Higor Segobia <higor.segobia@gmail.com>
2019-09-02 01:57:06 -0300

    Primeiro Commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Conversor from './components/Conversor'
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CONVERSOR DE MOEDAS</h1>
+      <div className="linha">
+      <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+      <Conversor moedaA="BRL" moedaB="USD"></Conversor>
+      </div>
+
+      <div className="linha">
+      <Conversor moedaA="CAD" moedaB="BRL"></Conversor>
+      <Conversor moedaA="BRL" moedaB="CAD"></Conversor>
+      </div>
+
+      <div className="linha">
+      <Conversor moedaA="EUR" moedaB="BRL"></Conversor>
+      <Conversor moedaA="BRL" moedaB="EUR"></Conversor>
+      </div>
+    </div>
+  );
+}
+
+export default App;
