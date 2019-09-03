9886bca53a8da787440d9233e6d44ac7c410a825
Higor Segobia <higor.segobia@gmail.com>
2019-09-03 10:46:43 -0300

    First Commit

--- src/components/Conversor.js
+++ src/components/Conversor.js
@@ -36,7 +36,7 @@ export default class Conversor extends Component {
         <div className="conversor">
             <h2>{this.props.moedaA} para {this.props.moedaB}</h2>
             <input type="text" onChange={(event)=>{this.setState({moedaA_valor:event.target.value})}}></input>
-            <input type="button" value="Converter" onClick={this.converter}></input>
+            <input type="button" value="Converter" className="button" onClick={this.converter}></input>
             <h2>{this.state.moedaB_valor}</h2>
 
         </div>
